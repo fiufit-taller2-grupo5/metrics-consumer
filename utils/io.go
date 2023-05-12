0508b80733d8485a5de50670b0bcb85ee384aad6
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-05-12 18:43:40 -0300

    Adds utils for reading files

---
+++ utils/io.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}(file)
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
